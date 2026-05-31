abc9bad80269c07b1155d3141b2f69298007a6f6
Jonnister <68484627+Jonnister@users.noreply.github.com>
2026-05-31 14:35:08 +0200

    Setup
    -Removed vite default code
    -Installed basic packages for backend and frontend
    -Added notes for frontend/baceknd design

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 // imports
 import express from "express";
+import cors from "cors";
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import session from 'express-session';
 
 // init express
 const app = new express();
@@ -9,3 +13,24 @@ const port = 3001;
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+//Databases
+//Stations: List of Station Names and positions (potentially?)
+//Lines-Stations: Contains List of Line Names associated with Stations 
+//Users username, email, password, salt, score
+//Events: Event ID (maybe not necessary), Event description, event coin modificator (from -4 to +4)
+
+//AUTHENTICATION
+//Set-> Login User
+//Delete -> Logout User
+//Get Session Storage -> Get user if logged in
+
+//Gameplay
+// Get All lines
+// Get all Stations
+// Get Random event (from -4 to +4 coins)
+// Get Random Starting and destination station (min distance 3)
+// Get List of Station Connections
+
+// Set Player Highscore
+
